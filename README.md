b6b86f3dfe975c13067c719676ba6fd9b984cc45
byt3h3ad <adhirajd007@gmail.com>
2025-03-18 00:21:34 +0530

    rip

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - scrapes from [Spotify Concerts page](https://open.spotify.com/concerts) every 24h
 
 - credits: [kognise](https://x.com/kognise7/status/1880391651455299813)
+
+- spotify shutdown the endpoints, archived.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import { cities } from "@/lib/utils";
-import Link from "next/link";
-
-export default async function Home() {
+export default function Home() {
+  const errorMessage = {
+    error: {
+      code: 400,
+      message: "Unauthorized request",
+      extra: {
+        _notes:
+          "Usage of this endpoint is not permitted under the Spotify Developer Terms and Developer Policy, and applicable law",
+      },
+    },
+  };
   return (
     <div className="grid items-center justify-center gap-6 min-h-screen p-8">
       <section className="grid gap-6 text-zinc-400 ">
-        <h1 className="text-zinc-300 text-xl py-6">Index</h1>
+        {/* <h1 className="text-zinc-300 text-xl py-6">Index</h1>
         {cities.map((city, index: number) => (
           <Link
             key={index}
@@ -14,7 +21,9 @@ export default async function Home() {
           >
             {city}
           </Link>
-        ))}
+        ))} */}
+        <p className="text-center">Spotify shutdown the endpoints :(</p>
+        <pre>{JSON.stringify(errorMessage, null, 2)}</pre>
       </section>
     </div>
   );
